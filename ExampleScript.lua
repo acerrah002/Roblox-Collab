98714d3bffebc5b9a6c03fe716f6daedf3a7365c
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2025-11-06 17:52:02 -0500

    Add files via upload

---
+++ ExampleScript.lua
@@ -0,0 +1,34 @@
+--Variable for a int
+Var = 1
+
+--Variable for string
+VarString = "One"
+
+--Variable for a boolean
+VarBoolean = false
+
+--This is a Int
+--print(1)
+--This is a float
+--print(1.0)
+--This is a String
+--print("1")
+--Boolean
+--print(true)
+--Table/List
+--print({1,2,3,4,5})
+--print(VarBoolean)
+--print("1+1")
+--print(1.5+1.5)
+
+--This is a function with 2 Parameters
+--Called A and B which are local
+--Variables to the function
+function addition(A, B)
+	print(A + B)
+end
+
+addition(5, 5)
+addition(2, 5)
+addition(6, 7)
+
