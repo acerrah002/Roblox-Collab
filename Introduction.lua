d363207e5953c7842584e6edba5e3393a6d02dd6
acerrah002 <151685429+acerrah002@users.noreply.github.com>
2025-11-13 17:16:32 -0500

    Update Introduction.lua

--- Introduction.lua
+++ Introduction.lua
@@ -1,3 +1,5 @@
 https://create.roblox.com/docs/tutorials/use-case-tutorials/scripting/basic-scripting/deadly-lava
 
 This is a test part 2
+Group
+https://www.roblox.com/communities/445669386/CreateCollabContinue#!/about
